0abf8a5df323487138f9a84331ebf98101503e1d
User <niedbalskiadrian@gmail.com>
2026-06-24 23:09:51 +0200

    Initial Spring Boot project

---
+++ src/test/java/org/lbooklibraryapi/booklibraryapi/BookLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lbooklibraryapi.booklibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
