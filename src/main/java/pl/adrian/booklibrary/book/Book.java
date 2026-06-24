115fcdf9a66e1e8e3fd000321314d442a1f841d8
User <niedbalskiadrian@gmail.com>
2026-06-24 23:49:11 +0200

    Fix book

--- src/main/java/pl/adrian/booklibrary/book/Book.java
+++ src/main/java/pl/adrian/booklibrary/book/Book.java
@@ -1,25 +1,23 @@
 package pl.adrian.booklibrary.book;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.annotations.Immutable;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 public class Book {
     @Id
-    @NotNull
+    @GeneratedValue
     private Long id;
     private String title;
-    @Immutable
     private String author;
     private BookStatus status;
     private LocalDateTime createdAt;
 
     public Book () {
         this.createdAt = LocalDateTime.now();
+        this.status = BookStatus.TO_READ;
     }
 }
